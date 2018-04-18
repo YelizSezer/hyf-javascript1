d9c2a80f7a302c01aa17d1e98da0ee067d98814a
Yeliz Sezer <yeliz.sezer@web.de>
2018-04-18 16:17:07 +0200

    Add files via upload

---
+++ week-1/homework1.js
@@ -0,0 +1,101 @@
+//exercise 1
+console.log("Hello World!"); //english
+console.log("Alo Dünya!"); //turkish
+console.log("Hallo Welt!"); //german
+
+//exercise 2
+console.log('Im awesome');
+//corrected
+console.log("I'm awesome");
+
+//exercise 3
+var x;
+console.log('the value of my variable x will be: an Integer');
+console.log(x);
+
+x=10;
+console.log('the value of my variable x will be 10');
+console.log(x);
+
+//exercise 4
+var y = "I am from Germany";
+console.log('the value of y will be: I am from Germany');
+console.log(y);
+
+y="I live in Malmö";
+console.log('the new value of the string will be: I live in Malmö');
+console.log(y);
+
+//exercise 5
+var z= 7.25;
+console.log(z);
+ var a = Math.round(7.25);
+ console.log(a);
+var b = Math.max(a,z);
+console.log(b);
+
+//exercise 6: arrays
+var plurality =[];
+console.log("empty array");
+console.log(plurality);
+
+var MyFavoriteAnimals =["dog","whale", "bird", "cat"];
+console.log(MyFavoriteAnimals); 
+
+MyFavoriteAnimals.push("baby pig");
+console.log(MyFavoriteAnimals); 
+
+//exercise 7: strings
+let myString = "this is a test";
+console.log(myString);
+
+myString.length;
+console.log(myString.length);
+
+//exercise 8
+let clock = 3;
+console.log('The value of the variable clock is: ' + 3);
+
+let sea="blue";
+console.log('The value of the variable sea is: ' + blue);
+
+let time=true;
+console.log('The value of the variable time is: ' + true);
+
+let arr = [1,2,3,4];
+console.log('The value of the variable arr is: ' + arr);
+
+
+//exercise 9
+let x = 7; 
+x % = 3 //x = x % 3;
+console.log(x);
+
+//exercise 9.1
+//example 1
+console.log(23 % 10);
+
+//example 2
+if ((10 % 2)===0){
+    console.log("the number is even")
+}
+else{
+    console.log("the first number is odd")
+}
+
+//example 3
+if ((23 % 10)===0){
+    console.log("the number is even")
+}
+else{
+    console.log("the first number is odd")
+}
+
+//10.
+var arr = [3,"ball","cat",6,7,8,12];
+console.log(arr);
+
+var employee = new Object();
+var books = new Array("C++", "Perl", "Java");
+var day = new Date("August 15, 1947");
+console.log(books);
